40c0e13443cc2a51bf4f2d45a607116b0cf52ea6
WiktorKociuba <wiktorkociubaw@gmail.com>
2026-02-28 18:13:35 +0100

    Fixed grzyb

--- game_controller.gd
+++ game_controller.gd
@@ -1,5 +1,5 @@
 extends Node
 var total_coins: int=0
 func coin_collected(value: int):
-	total_coins+=value
-	EventController.emit_signal("coin_collected", total_coins)
+    total_coins+=value
+    EventController.emit_signal("coin_collected", total_coins)
