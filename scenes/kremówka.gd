6403a0a10a4f938574e9c412a9ad5a9c2141756c
WiktorKociuba <wiktorkociubaw@gmail.com>
2026-02-28 16:43:52 +0100

    added kremówka

--- scenes/kremówka.gd
+++ scenes/kremówka.gd
@@ -3,3 +3,9 @@ extends Sprite2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     pass
+
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+    if event is InputEventMouseButton and event.pressed:
+        visible = false
