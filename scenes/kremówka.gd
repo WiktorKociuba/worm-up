031c5785c0ccb30a0e9834fa6e92146d7cda8a29
WiktorKociuba <wiktorkociubaw@gmail.com>
2026-02-28 17:33:44 +0100

    Added barka

--- scenes/kremówka.gd
+++ scenes/kremówka.gd
@@ -8,4 +8,10 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
     if event is InputEventMouseButton and event.pressed:
-        visible = false
+        $"Area2D/Kremówka".visible = false
+        $barka.play()
+        $Timer.start(5.3)
+
+func _on_timer_timeout() -> void:
+    $"../ProductItemList".visible = true
+    visible = false
