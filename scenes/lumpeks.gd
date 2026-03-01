eb18a2f0a7e61f99f69bd6386acea21dfb1edd88
WiktorKociuba <wiktorkociubaw@gmail.com>
2026-03-01 16:01:38 +0100

    fixed the exit from different places

--- scenes/lumpeks.gd
+++ scenes/lumpeks.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
-		var location=Player.global_position
-		get_tree().change_scene_to_file("res://scenes/wardrobe.tscn")
-		
-		
+    if body is Player:
+        GameController.globalPos = body.position
+        get_tree().change_scene_to_file("res://scenes/wardrobe.tscn")
+        
+        
+
+func _ready() -> void:
+    $Area2D.monitoring = false
+    $Area2D.monitorable = false
+func _on_timer_timeout() -> void:
+    $Area2D.monitoring = true
+    $Area2D.monitorable = true
