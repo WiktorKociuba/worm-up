38f34c9135095742177137862a5770b3cf6c63eb
madziunianwk <madziunianwk@gmail.com>
2026-03-01 18:25:05 +0100

    .

--- scenes/fryzjer.gd
+++ scenes/fryzjer.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
 func _ready() -> void:
-    $Area2D.monitoring = false
-    $Area2D.monitorable = false
+	$Area2D.monitoring = false
+	$Area2D.monitorable = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-    if body is Player:
-        GameController.globalPos = body.position
-        QuestController.deleteQuest.emit(4)
-        get_tree().change_scene_to_file("res://scenes/hair.tscn")
+	if body is Player:
+		GameController.globalPos = body.position
+		QuestController.deleteQuest.emit(4)
+		get_tree().change_scene_to_file("res://scenes/hair.tscn")
 
 func _on_timer_timeout() -> void:
-    $Area2D.monitoring = true
-    $Area2D.monitorable = true
+	$Area2D.monitoring = true
+	$Area2D.monitorable = true
