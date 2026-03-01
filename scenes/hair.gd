e3a5c42534e8c6568ba5870e602e26de6fe348fd
madziunianwk <madziunianwk@gmail.com>
2026-03-01 12:21:27 +0100

    animation

--- scenes/hair.gd
+++ scenes/hair.gd
@@ -3,17 +3,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    pass
+	pass
 
 
 func _on_button_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_head_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
-    Clothes.head = index
-    Clothes.clothesChanged.emit()
+	Clothes.head = index
+	Clothes.clothesChanged.emit()
