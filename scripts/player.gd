a30896f306688560f069ec31bb352cd5a3149127
WiktorKociuba <wiktorkociubaw@gmail.com>
2026-03-01 15:26:30 +0100

    fixed clothes

--- scripts/player.gd
+++ scripts/player.gd
@@ -83,28 +83,28 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
     pass # Replace with function body.
 
 func displayNewDialog(who: int,text: String, id: int):
-	if id == -1:
-		$UI/DialogUI.visible = false
-		disableMovement = false
-		dialogId = id
-		return
-	if id == 0:
-		disableMovement = true
-		$UI/DialogUI.visible = true
-	$UI/DialogUI/DialogBox/DialogText.text = text
-	for icon in npcIcons:
-		icon.visible = false
-	npcIcons[who].visible = true
-	dialogWho = who
-	dialogId = id
-	
+    if id == -1:
+        $UI/DialogUI.visible = false
+        disableMovement = false
+        dialogId = id
+        return
+    if id == 0:
+        disableMovement = true
+        $UI/DialogUI.visible = true
+    $UI/DialogUI/DialogBox/DialogText.text = text
+    for icon in npcIcons:
+        icon.visible = false
+    npcIcons[who].visible = true
+    dialogWho = who
+    dialogId = id
+    
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		DialogController.emit_signal("nextDialog", dialogWho, dialogId)
+    if event is InputEventMouseButton and event.pressed:
+        DialogController.emit_signal("nextDialog", dialogWho, dialogId)
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
-	
+    get_tree().change_scene_to_file("res://scenes/main.tscn")
+    
 func addQuest(id: int, text: String):
     var questLabel = Label.new()
     QuestController.questTexts[id] = text
