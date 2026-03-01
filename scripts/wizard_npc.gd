e98a7741b4afe5559bade9376c11af47324c3988
WiktorKociuba <wiktorkociubaw@gmail.com>
2026-03-01 11:16:09 +0100

    added wizard dialog

--- scripts/wizard_npc.gd
+++ scripts/wizard_npc.gd
@@ -2,13 +2,17 @@ class_name wizard extends CharacterBody2D
 
 @export var dialogs: Array[String] = []
 @export var dialogsWho: Array[int] = []
-
+var dialogDisplayed: bool = false
 func _ready() -> void:
     DialogController.nextDialog.connect(displayNextDialog)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-    DialogController.emit_signal("displayDialog",dialogsWho[0],dialogs[0],0)
+    if body is Player and not dialogDisplayed:
+        DialogController.emit_signal("displayDialog",dialogsWho[0],dialogs[0],0)
+        dialogDisplayed = true
     
 func displayNextDialog(who: int, id: int):
     if id+1 < dialogs.size():
         DialogController.emit_signal("displayDialog",dialogsWho[id+1],dialogs[id+1],id+1)
+    else:
+        DialogController.emit_signal("displayDialog",-1,"",-1)
